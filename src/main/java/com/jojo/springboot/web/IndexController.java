9f70f07671aa68b122d1214ef4904b3d891cbc1d
kiiooxx <58679302+kiiooxx@users.noreply.github.com>
2020-03-06 16:09:42 +0900

    네이버 로그인 연동 구현

--- src/main/java/com/jojo/springboot/web/IndexController.java
+++ src/main/java/com/jojo/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojo.springboot.web;
 
+import com.jojo.springboot.config.auth.LoginUser;
 import com.jojo.springboot.config.auth.dto.SessionUser;
 import com.jojo.springboot.service.posts.PostsService;
 import com.jojo.springboot.web.dto.PostsResponseDto;
@@ -16,12 +17,11 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if(user != null) {
             model.addAttribute("userName", user.getName());
         }
